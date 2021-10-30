73a321408ddcab8dc1cbdb8781e34fd12dea17d3
BlueBug12 <benson.liu.himself@gmail.com>
2021-10-30 09:11:00 +0000

    test2 py

--- hw3/BlueBug12/test_matrix.py
+++ hw3/BlueBug12/test_matrix.py
@@ -48,9 +48,9 @@ class TestMatrix:
         
     def write_file(self,file_name):
         with open(file_name,'w') as f:
-            f.write('naive method takes '+str(self.naive_time) + "s"\n)
-            f.write('MKL method takes '+str(self.mkl_time) + "s (x" + str(self.naive_time/self.mkl_time))
-            f.write('tiling method takes '+str(self.tile_time) + "s (x" + str(self.naive_time/self.tile_time))
+            f.write('naive method takes '+str(self.naive_time) + "s\n")
+            f.write('MKL method takes '+str(self.mkl_time) + "s (x" + str(self.naive_time/self.mkl_time)+"\n")
+            f.write('tiling method takes '+str(self.tile_time) + "s (x" + str(self.naive_time/self.tile_time)+"\n)
 
     def test_matrix(self):
         multiplier(1024,1024,1024,8)
